76eeaea7d1d080833a21f3c0368cb6dd58146f0e
Twaiz <homenkobodya@gmail.com>
2024-09-17 19:48:30 +0300

    add[stylization AppLayout]

--- src/ui/applayout/AppLayout.tsx
+++ src/ui/applayout/AppLayout.tsx
@@ -1,16 +1,18 @@
 import { Outlet } from 'react-router-dom';
 
+import styles from './AppLayout.module.scss';
+
 import Header from '@/components/Header/Header';
 
 const AppLayout = () => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
 
-      <main>
+      <main className={styles.main}>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
